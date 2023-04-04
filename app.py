ffd1cc4a50ef4aa3ab19c161ef8c585e4dc75df9
praveenjutur <praveen4dream@gmail.com>
2023-04-04 11:21:33 +0000

    commit 2

--- app.py
+++ app.py
@@ -1,41 +1,30 @@
-# from flask import Flask, render_template
-# app = Flask(__name__)
-
-# @app.route("/")
-# def home():
-#     return render_template("home.html")
-
-# if __name__ == "__main__":
-#     app.run(debug=True, host="0.0.0.0", port=3000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# """Flask application for Paws Rescue Center."""
-from flask import Flask,render_template
+"""Flask Application for Paws Rescue Center."""
+from flask import Flask, render_template
 app = Flask(__name__)
 
-"""View function for home page"""
+"""Information regarding the Pets in the System."""
+pets = [
+            {"id": 1, "name": "Nelly", "age": "5 weeks", "bio": "I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles."},
+            {"id": 2, "name": "Yuki", "age": "8 months", "bio": "I am a handsome gentle-cat. I like to dress up in bow ties."},
+            {"id": 3, "name": "Basker", "age": "1 year", "bio": "I love barking. But, I love my friends more."},
+            {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."}, 
+        ]
+
 @app.route("/")
 def homepage():
-    return render_template('home.html')
+    """View function for Home Page."""
+    return render_template("home.html", pets = pets)
+
+@app.route("/details/<pet_id>")
+def details(pet_id):
+    
 
 
-"""View function for about page"""
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    """View function for About Page."""
+    return render_template("about.html")
+
 
 
 if __name__ == "__main__":
